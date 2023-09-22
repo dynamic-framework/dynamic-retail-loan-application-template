e8bba01db16b09421662ac1dec4a73b929166139
Johann Felipe Rojas <johannrip@gmail.com>
2023-09-22 12:39:17 -0500

    refactor: update path name

--- src/config/widgetConfig.ts
+++ src/config/widgetConfig.ts
@@ -4,7 +4,7 @@ export const SITE_URL = liquidParser.parse('{{site.url}}');
 export const USER_ID = liquidParser.parse('{{user.id}}');
 
 export const SITE_PATH = {
-  DASHBOARD: liquidParser.parse('{{vars.path-dashboard}}'),
+  DASHBOARD: liquidParser.parse('{{vars.dashboard-path}}'),
 };
 
 export type SitePath = keyof typeof SITE_PATH;
