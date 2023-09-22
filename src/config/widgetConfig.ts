95abdd72b6456296f1c0f8a6ea792f6c548787ad
Johann Felipe Rojas <johannrip@gmail.com>
2023-09-21 20:29:45 -0500

    chore: util to go path

---
+++ src/config/widgetConfig.ts
@@ -0,0 +1,10 @@
+import { liquidParser } from '@dynamic-framework/ui-react';
+
+export const SITE_URL = liquidParser.parse('{{site.url}}');
+export const USER_ID = liquidParser.parse('{{user.id}}');
+
+export const SITE_PATH = {
+  DASHBOARD: liquidParser.parse('{{vars.path-dashboard}}'),
+};
+
+export type SitePath = keyof typeof SITE_PATH;
