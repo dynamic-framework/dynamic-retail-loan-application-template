bb0b032bbb2cfea5b9b7fe6244bf7e1307885d8f
Johann Felipe Rojas <johannrip@gmail.com>
2024-03-04 23:41:43 -0500

    chore: add effect to init context with liquid

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import { useDContext } from '@dynamic-framework/ui-react';
+import { useEffect } from 'react';
 import ApplicationStatus from './components/ApplicationStatus';
 import CreditSimulation from './components/CreditSimulation';
 import SkeletonLoader from './components/SkeletonLoader';
 import { useAppSelector } from './store/hooks';
 import { getStatus } from './store/selectors';
 import useLoanRequestEffect from './services/hooks/useLoanRequestEffect';
+import { SITE_LANG, VARS_CURRENCY } from './config/widgetConfig';
 
 export default function App() {
   const status = useAppSelector(getStatus);
   const { loading } = useLoanRequestEffect();
+  const { setContext } = useDContext();
+
+  useEffect(() => {
+    setContext({
+      language: SITE_LANG,
+      currency: VARS_CURRENCY,
+    });
+  }, [setContext]);
 
   return (
     <div className="container py-3">
