5d0c159f1839510ee92c9cc8ba68f28653f7ea35
Eduard Baron <ebaron@modyo.com>
2024-08-02 13:53:06 -0500

    fix: user id from liquid vars

--- src/config/widgetConfig.ts
+++ src/config/widgetConfig.ts
@@ -2,7 +2,7 @@ import liquidParser from '../utils/liquidParser';
 
 export const SITE_URL = liquidParser.parse('{{site.url}}');
 export const SITE_LANG = liquidParser.parse('{{site.language}}');
-export const USER_ID = liquidParser.parse('{{user.id}}');
+export const USER_ID = liquidParser.parse('{{vars.user-id}}');
 
 export const VARS_CURRENCY = {
   symbol: liquidParser.parse('{{vars.currency-symbol}}'),
